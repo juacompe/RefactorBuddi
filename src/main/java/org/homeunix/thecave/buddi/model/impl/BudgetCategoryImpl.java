fd78862b3d808fbadec181d5876700a999fde1e2
juacompe <juacompe@gmail.com>
2020-11-16 17:16:15 +0700

    :recycle: Removed commented code

--- src/main/java/org/homeunix/thecave/buddi/model/impl/BudgetCategoryImpl.java
+++ src/main/java/org/homeunix/thecave/buddi/model/impl/BudgetCategoryImpl.java
@@ -105,16 +105,10 @@ public class BudgetCategoryImpl extends SourceImpl implements BudgetCategory {
 		//If Start and End are in the same budget period
 		if (getBudgetPeriodType().getStartOfBudgetPeriod(startDate).equals(
 				getBudgetPeriodType().getStartOfBudgetPeriod(endDate))){
-//			Logger.getLogger().info("Start Date and End Date are in the same period.");
 			long amount = getAmount(startDate);
-//			Logger.getLogger().info("Amount = " + amount);
 			long daysInPeriod = getBudgetPeriodType().getDaysInPeriod(startDate);
-//			Logger.getLogger().info("Days in Period = " + daysInPeriod);
 			long daysBetween = DateUtil.getDaysBetween(startDate, endDate, true);
-//			Logger.getLogger().info("Days Between = " + daysBetween);
-		
-//			Logger.getLogger().info("Returning " + (long) (((double) amount / (double) daysInPeriod) * daysBetween));
-//			Logger.getLogger().info("Finished calculating the budget amount.\n\n");
+
 			return (long) (((double) amount / (double) daysInPeriod) * daysBetween);
 		}
 		 
@@ -123,16 +117,11 @@ public class BudgetCategoryImpl extends SourceImpl implements BudgetCategory {
 				getBudgetPeriodType().getStartOfBudgetPeriod(endDate))
 				|| getBudgetPeriodType().getBudgetPeriodOffset(startDate, 1).before(
 						getBudgetPeriodType().getStartOfBudgetPeriod(endDate))){
-//			Logger.getLogger().info("Start Date and End Date are in different budget periods.");
 			long amountStartPeriod = getAmount(startDate);
-//			Logger.getLogger().info("Amount Start Period = " + amountStartPeriod);
 			long daysInStartPeriod = getBudgetPeriodType().getDaysInPeriod(startDate);
-//			Logger.getLogger().info("Days in Start Period = " + daysInStartPeriod);
 			long daysAfterStartDateInStartPeriod = DateUtil.getDaysBetween(startDate, getBudgetPeriodType().getEndOfBudgetPeriod(startDate), true);
-//			Logger.getLogger().info("Days After Start Date in Start Period = " + daysAfterStartDateInStartPeriod);
 			double totalStartPeriod = (((double) amountStartPeriod / (double) daysInStartPeriod) * daysAfterStartDateInStartPeriod);
-//			Logger.getLogger().info("Total in Start Period = " + totalStartPeriod);
-			
+
 			double totalInMiddle = 0;
 			for (String periodKey : getBudgetPeriods(
 					getBudgetPeriodType().getBudgetPeriodOffset(startDate, 1),
@@ -140,19 +129,11 @@ public class BudgetCategoryImpl extends SourceImpl implements BudgetCategory {
 				totalInMiddle += getAmount(getPeriodDate(periodKey));
 				Logger.getLogger(this.getClass().getName()).info("Added " + getAmount(getPeriodDate(periodKey)) + " to total for one period in between; current value is " + totalInMiddle);
 			}
-//			Logger.getLogger().info("Total in Middle = " + totalInMiddle);
-			
+
 			long amountEndPeriod = getAmount(endDate);
-//			Logger.getLogger().info("Amount End Period = " + amountEndPeriod);
 			long daysInEndPeriod = getBudgetPeriodType().getDaysInPeriod(endDate);
-//			Logger.getLogger().info("Days in End Period = " + daysInEndPeriod);
 			long daysBeforeEndDateInEndPeriod = DateUtil.getDaysBetween(getBudgetPeriodType().getStartOfBudgetPeriod(endDate), endDate, true);
-//			Logger.getLogger().info("Days before End Period = " + daysBeforeEndDateInEndPeriod);
-			double totalEndPeriod = (long) (((double) amountEndPeriod / (double) daysInEndPeriod) * daysBeforeEndDateInEndPeriod); 
-//			Logger.getLogger().info("Total in End Period = " + totalEndPeriod);
-			
-//			Logger.getLogger().info("Sum of Start Period, Middle, and End Period = " + (totalStartPeriod + totalInMiddle + totalEndPeriod));
-//			Logger.getLogger().info("Finished Calculating the Budget Amount\n\n");
+			double totalEndPeriod = (long) (((double) amountEndPeriod / (double) daysInEndPeriod) * daysBeforeEndDateInEndPeriod);
 			return (long) (totalStartPeriod + totalInMiddle + totalEndPeriod);
 		}
 
